83b4750b0c01c2977cde057352da52919df418bf
Shyam <shyamkadam8390@gmail.com>
2026-07-16 11:33:16 +0530

    commit 11

--- tests/apipostcall.spec.ts
+++ tests/apipostcall.spec.ts
@@ -3,7 +3,7 @@ import {test,expect} from '@playwright/test';
 test('Test post api',async ({request})=>{
     const accessToken: string = "1b0dd70f56603908262098f5dc3fbf5f018d6e02160633a5480aec4f99b43ea6";
     const response = await request.post("https://simple-books-api.click/orders",{headers:{Authorization: `Bearer ${accessToken}`},data:{bookId: "3",
-    "customerName": "Shyam2"}});
+    "customerName": "Shyam10"}});
     console.log(response.status());
     //await expect(response).toBeOK();
     const responseBody = await response.json();
