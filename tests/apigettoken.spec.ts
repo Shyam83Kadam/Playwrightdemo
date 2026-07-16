15053294a39b3040ca2984870381666fdbca0f75
Shyam <shyamkadam8390@gmail.com>
2026-07-16 11:26:36 +0530

    commit

--- tests/apigettoken.spec.ts
+++ tests/apigettoken.spec.ts
@@ -1,6 +1,7 @@
 import {test,expect} from '@playwright/test';
 
 test('Test get token',async ({request})=>{
+    // generate api token
     const response = await request.post("https://simple-books-api.click/api-clients/",{headers:{"content-type":"application/json"},data:{"clientName": "Shyam",
    "clientEmail": "ashu@gmail.com"}});
     console.log(response.status());
